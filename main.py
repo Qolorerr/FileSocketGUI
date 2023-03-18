eee12d39c352c48c056b7e80ffd53b630ee59a6b
Qolorerr <56872573+Qolorerr@users.noreply.github.com>
2023-03-18 16:20:19 +0300

    Choose PC dialog

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
-
+from typing import Tuple
 from PyQt6 import uic
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from qt_material import apply_stylesheet
-from filesocket import CONFIG_FILE, LOGGER_CONFIG
+from filesocket import CONFIG_FILE, LOGGER_CONFIG, ManagingClient
 from filesocket.storekeeper import Storekeeper
 
+from src.choose_pc_dialog import ChoosePCDialog
 from src.signin_dialog import SigninDialog
 
 
@@ -14,8 +15,23 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi("res/ui/main_window.ui", self)
 
-        store_keeper = Storekeeper(LOGGER_CONFIG, CONFIG_FILE)
+        self.signin()
 
+        pc_id, device_secure_token = self.choose_pc()
+        self.client = ManagingClient(pc_id, device_secure_token)
+        self.client.run(False)
+
+        self.show()
+
+        # TODO: UI of console tab
+        # TODO: UI of scripts tab
+
+        # TODO: Realisation of tabs backend
+
+    # Sign in and sign up
+    @staticmethod
+    def signin() -> None:
+        store_keeper = Storekeeper(LOGGER_CONFIG, CONFIG_FILE)
         state = 1
         while not store_keeper.get_token() and state:
             window = SigninDialog()
@@ -23,14 +39,32 @@ class MainWindow(QMainWindow):
         if state == 0:
             sys.exit()
 
-        # TODO: Show list of PCs
+    # Choose PC to connect
+    @staticmethod
+    def choose_pc() -> Tuple[int, str] | Tuple[int, None]:
+        result = ChoosePCDialog().execute()
+        if result is None:
+            sys.exit()
+        return result
 
-        # self.show()
+    def get_device_secure_token(self, pc_id: int, secure_token: str | None) -> str | None:
+        store_keeper = Storekeeper(LOGGER_CONFIG, CONFIG_FILE)
+        try:
+            secure_tokens = store_keeper.get_value("secure_token")
+        except KeyError:
+            secure_tokens = dict()
+            store_keeper.add_value("secure_token", secure_tokens)
+        if secure_token is not None:
+            self.set_device_secure_token(pc_id, secure_token)
+            return secure_token
+        return secure_tokens[str(pc_id)] if str(pc_id) in secure_tokens else None
 
-        # TODO: UI of console tab
-        # TODO: UI of scripts tab
-
-        # TODO: Realisation of tabs backend
+    @staticmethod
+    def set_device_secure_token(pc_id: int, secure_token: str) -> None:
+        store_keeper = Storekeeper(LOGGER_CONFIG, CONFIG_FILE)
+        secure_tokens = store_keeper.get_value("secure_token")
+        secure_tokens[str(pc_id)] = secure_token
+        store_keeper.add_value("secure_token", secure_tokens)
 
 
 if __name__ == "__main__":
---
+++ src/choose_pc_dialog.py
@@ -0,0 +1,49 @@
+from typing import Tuple
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import QDialog, QPushButton, QListWidgetItem
+from filesocket import show_all_pc, ManagingClient, ServerError, PCEntity
+
+
+class ChoosePCDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("res/ui/choose_pc_dialog.ui", self)
+        self.device_id = None
+        self.load_table()
+
+    @staticmethod
+    def _check_online(pc: PCEntity) -> bool:
+        temp_client = ManagingClient(pc.id)
+        temp_client.run(False)
+        try:
+            return temp_client.check_online()
+        except ServerError as e:
+            print(e)
+            return False
+
+    def load_table(self) -> None:
+        all_pc = show_all_pc()
+        availability = tuple(map(self._check_online, all_pc))
+        all_pc = list(zip(all_pc, availability))
+        all_pc.sort(key=lambda pc: pc[1], reverse=True)
+        for pc in all_pc:
+            button = QPushButton()
+            button.setObjectName(f"button_{pc[0].id}")
+            button.setText(pc[0].name)
+            button.setEnabled(pc[1])
+            item = QListWidgetItem(self.table)
+            self.table.addItem(item)
+            self.table.setItemWidget(item, button)
+            button.clicked.connect(self.open_connection)
+
+    def open_connection(self):
+        self.device_id = int(self.sender().objectName().removeprefix("button_"))
+        self.accept()
+
+    def execute(self) -> Tuple[int, str] | Tuple[int, None] | None:
+        state = self.exec()
+        if state == 0:
+            return None
+        secure_token = None if self.secure_token.text().strip() == '' else self.secure_token.text().strip()
+        return self.device_id, secure_token
